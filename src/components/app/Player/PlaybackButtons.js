79747253d3726a6e852793743e92d0b3cf291e54
Eric Hicks <ehicks05@yahoo.com>
2021-03-21 23:27:40 -0400

    clean up

--- src/components/app/Player/PlaybackButtons.js
+++ src/components/app/Player/PlaybackButtons.js
@@ -1,24 +1,12 @@
 import React from "react";
-import {
-  useUserStore,
-  setSelectedTrackId,
-} from "../../../common/UserContextProvider";
-import {
-  useAppStore,
-  getPlaylistById,
-} from "../../../common/AppContextProvider";
+import { setSelectedTrackId } from "../../../common/UserContextProvider";
 import { useTimeStore } from "../../../common/TimeContextProvider";
 
 import { useWindowSize } from "react-use";
 import { FaPause, FaPlay, FaStepBackward, FaStepForward } from "react-icons/fa";
-
-const prevButtonStyle = { height: "36px", width: "36px" };
-const pauseButtonStyle = { height: "45px", width: "45px" };
-const nextButtonStyle = { height: "36px", width: "36px" };
+import { getNewTrackId } from "./utils";
 
 export default function PlaybackButtons() {
-  const user = useUserStore((state) => state.user);
-  const tracks = useAppStore((state) => state.tracks);
   const playerState = useTimeStore((state) => state.playerState);
   const setPlayerState = useTimeStore((state) => state.setPlayerState);
   const { width } = useWindowSize();
@@ -32,99 +20,39 @@ export default function PlaybackButtons() {
     setSelectedTrackId(newTrackId);
   }
 
-  function getCurrentPlaylistTrackIds() {
-    const currentPlaylist = getPlaylistById(user.userState.selectedPlaylistId);
-    if (currentPlaylist)
-      return currentPlaylist.playlistTracks.map(
-        (playlistTrack) => playlistTrack.track.id
-      );
-    else return tracks.map((track) => track.id);
-  }
-
-  function getNewTrackId(input) {
-    const currentPlaylistTrackIds = getCurrentPlaylistTrackIds();
-    let newTrackId = -1;
-    const shuffle = user.userState.shuffle;
-    if (shuffle) {
-      let newPlaylistTrackIndex = Math.floor(
-        Math.random() * currentPlaylistTrackIds.length
-      );
-      newTrackId = currentPlaylistTrackIds[newPlaylistTrackIndex];
-      console.log("new random trackId: " + newTrackId);
-    } else {
-      const currentTrackIndex = currentPlaylistTrackIds.indexOf(
-        user.userState.selectedTrackId
-      );
-
-      let newIndex;
-      if (input === "prev") {
-        newIndex = currentTrackIndex - 1;
-        if (newIndex < 0) {
-          newIndex = currentPlaylistTrackIds.length - 1;
-        }
-      }
-      if (input === "next") {
-        newIndex = currentTrackIndex + 1;
-        if (newIndex >= currentPlaylistTrackIds.length) {
-          newIndex = 0;
-        }
-      }
-
-      newTrackId = currentPlaylistTrackIds[newIndex];
-    }
-
-    if (newTrackId === -1) {
-      console.error("Unable to select a new track id.");
-    }
-
-    return newTrackId;
-  }
-
-  const prevButton = (
-    <button
-      className="button"
-      style={prevButtonStyle}
-      onClick={() => handleTrackChange("prev")}
-    >
-      <span className="icon">
-        <FaStepBackward />
-      </span>
-    </button>
-  );
-
-  const playButton = (
-    <button
-      className="button is-medium"
-      style={pauseButtonStyle}
-      onClick={() =>
-        handlePlayerStateChange(
-          playerState === "playing" ? "paused" : "playing"
-        )
-      }
-    >
-      <span className="icon">
-        {playerState === "playing" ? <FaPause /> : <FaPlay />}
-      </span>
-    </button>
-  );
-
-  const nextButton = (
-    <button
-      className="button"
-      style={nextButtonStyle}
-      onClick={() => handleTrackChange("next")}
-    >
-      <span className="icon">
-        <FaStepForward />
-      </span>
-    </button>
-  );
-
   return (
     <>
-      {prevButton}
-      {playButton}
-      {nextButton}
+      <button
+        className="button"
+        style={{ height: "36px", width: "36px" }}
+        onClick={() => handleTrackChange("prev")}
+      >
+        <span className="icon">
+          <FaStepBackward />
+        </span>
+      </button>
+      <button
+        className="button is-medium"
+        style={{ height: "45px", width: "45px" }}
+        onClick={() =>
+          handlePlayerStateChange(
+            playerState === "playing" ? "paused" : "playing"
+          )
+        }
+      >
+        <span className="icon">
+          {playerState === "playing" ? <FaPause /> : <FaPlay />}
+        </span>
+      </button>
+      <button
+        className="button"
+        style={{ height: "36px", width: "36px" }}
+        onClick={() => handleTrackChange("next")}
+      >
+        <span className="icon">
+          <FaStepForward />
+        </span>
+      </button>
       {width >= 768 && <span style={{ paddingLeft: "8px" }}> </span>}
     </>
   );
