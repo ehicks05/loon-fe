2356efad2a1ceaef14fff09b13988118744dda72
Eric Hicks <ehicks05@yahoo.com>
2021-03-28 17:14:14 -0400

    add .env

--- src/common/SuperFetch.js
+++ src/common/SuperFetch.js
@@ -1,5 +1,6 @@
+const baseUrl = process.env.REACT_APP_URL;
 function superFetch(url, options) {
-  return fetch(url, options);
+  return fetch(baseUrl + "/" + url, options);
 }
 
 export default superFetch;
