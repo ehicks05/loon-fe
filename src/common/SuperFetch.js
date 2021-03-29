6f1249d67499d491e9fcecdc3f992a036c5838b3
Eric Hicks <ehicks05@yahoo.com>
2021-03-28 20:51:47 -0400

    credentials

--- src/common/SuperFetch.js
+++ src/common/SuperFetch.js
@@ -1,7 +1,7 @@
 const apiUrl =
   process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "";
 
-function superFetch(url, options) {
+function superFetch(url, { credentials: include, ...options }) {
   return fetch(apiUrl + url, options);
 }
 
